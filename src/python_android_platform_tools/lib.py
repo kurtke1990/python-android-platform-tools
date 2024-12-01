2db08ca3b3aed09ad2a18734b5a6ea0bdfdac101
Kurt Ke <kurtke1990@gmail.com>
2024-12-01 13:57:54 +0800

    feat: add lib.py to store all generic functions

---
+++ src/python_android_platform_tools/lib.py
@@ -0,0 +1,11 @@
+import re
+
+
+def remove_empty_strings(strings: list[str]) -> list[str]:
+    return [s for s in strings if s is not None and s.strip()]
+
+
+def search_by_pattern(src_string: str, pattern: str, group_num: int) -> re.Match | None:
+    p = re.compile(pattern)
+    matched = p.search(src_string)
+    return matched.group(group_num) if matched else None
