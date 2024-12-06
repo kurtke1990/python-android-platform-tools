38347139cf7ea7292ee095101e67db89195c6be9
Kurt Ke <kurtke1990@gmail.com>
2024-12-06 16:09:28 +0800

    feat: add a function to stop the adb server

--- test/adb/test_server.py
+++ test/adb/test_server.py
@@ -1,4 +1,4 @@
-from python_android_platform_tools.adb.server import start_server
+from python_android_platform_tools.adb.server import start_server, stop_server
 
 
 def test_start_server(subprocess_run_stub, assert_subprocess_run_called_with):
@@ -6,4 +6,11 @@ def test_start_server(subprocess_run_stub, assert_subprocess_run_called_with):
     port = "5037"
     cmd = f"adb -P {port} start-server"
     start_server(port)
+    assert_subprocess_run_called_with(run_stub, cmd, timeout=10)
+
+
+def test_stop_server(subprocess_run_stub, assert_subprocess_run_called_with):
+    run_stub = subprocess_run_stub("", "", returncode=0)
+    cmd = "adb kill-server"
+    stop_server()
     assert_subprocess_run_called_with(run_stub, cmd, timeout=10)
