c5e7d27584958321394cfd81364b8d010108bc81
Kurt Ke <kurtke1990@gmail.com>
2024-12-06 16:15:20 +0800

    feat: make some functions public

--- src/python_android_platform_tools/__init__.py
+++ src/python_android_platform_tools/__init__.py
@@ -1,5 +1,9 @@
-from python_android_platform_tools.adb.client import get_attached_devices
+from python_android_platform_tools.adb.client import get_attached_devices, wait_for_device_attached
+from python_android_platform_tools.adb.server import start_server, stop_server
 
 __all__ = [
     "get_attached_devices",
+    "wait_for_device_attached",
+    "start_server",
+    "stop_server",
 ]
