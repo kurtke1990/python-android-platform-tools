6b75a0ba87d8ead835d753c1c964b5fc9a1eaf36
Kurt Ke <kurtke1990@gmail.com>
2024-12-07 14:16:47 +0800

    style: fix type hint

--- src/python_android_platform_tools/adb/server.py
+++ src/python_android_platform_tools/adb/server.py
@@ -1,11 +1,11 @@
 from python_android_platform_tools.adb.common import execute_command
 
 
-def start_server(port: str = "5037"):
+def start_server(port: str = "5037") -> None:
     cmd = f"-P {port} start-server"
     execute_command(cmd=cmd, is_adb_shell=False, timeout=10)
 
 
-def stop_server():
+def stop_server() -> None:
     cmd = "kill-server"
     execute_command(cmd=cmd, is_adb_shell=False, timeout=10)
